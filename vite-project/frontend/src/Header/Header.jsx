801b34f2bbdb5ff7ad8e210139db205b49f600dd
xavcramer <artiomkoz712@gmail.com>
2025-10-22 22:16:56 +0200

    добавил мейн

--- vite-project/frontend/src/Header/Header.jsx
+++ vite-project/frontend/src/Header/Header.jsx
@@ -7,14 +7,14 @@ import headerhuman3 from "./imgs/headerhuman3.png"
 export default function Header() {
     return (
         <>
-            <header className='w-[full] h-[fit]'>
-                <div className='w-[full] h-[900px] bg-header-img bg-cover bg-center relative z-1'>
-                    <div class='w-[74%] h-[700px] ml-[13%] mr-[13%]'>
-                        <div class='w-[full] h-[790px] md:flex items-center'>
-                            <div class='w-[full] h-[full] md:flex-wrap'>
-                                <h1 class='w-[576px] h-[fit] text-[49px] leading-[65px] text-graytext'>The <span class='text-salad underline'>most complete </span>Photography resource.</h1>
-                                <p class='w-[500px] h-[fit] text-[19px] mt-[4px] text-graytext'>Welcome at Template Resource. This template is packed with useful features to built out your own resource website.</p>
-                                <div class='w-[500px] h-[full] md:flex gap-x-[20px] justify-center items-center mt-[3vh]'>
+            <header className='w-full h-fit'>
+                <div className='w-full h-[900px] bg-header-img bg-cover bg-center relative z-1'>
+                    <div class='w-[60%] h-[700px] ml-[20%] mr-[20%]'>
+                        <div class='w-full h-[790px] md:flex items-center'>
+                            <div class='w-full h-fit md:flex-wrap'>
+                                <h1 class='w-[576px] h-fit text-[49px] leading-[65px] text-graytext'>The <span class='text-salad underline'>most complete </span>Photography resource.</h1>
+                                <p class='w-[500px] h-fit text-[19px] mt-[4px] text-graytext'>Welcome at Template Resource. This template is packed with useful features to built out your own resource website.</p>
+                                <div class='w-[500px] h-fit md:flex gap-x-[20px] items-center mt-[10vh]'>
                                     <button class='w-[211px] h-[63px] bg-orange text-white text-[23px] p-[10px] leading-none cursor-pointer'>Visit our Blog <br /><span class='text-[14px]'>Stories, News & info</span></button>
                                     <button class='w-[211px] h-[63px] bg-white text-graytext text-[23px] p-[10px] leading-none cursor-pointer'>Resource Center <br /><span class='text-[14px]'>Tips, Tricks & Tutorials</span></button>
                                 </div>
@@ -23,7 +23,7 @@ export default function Header() {
 
                     </div>
                 </div>
-                <div class='w-[78%] h-[fit] ml-[11%] mr-[11%]'>
+                <div class='w-[74%] h-fit ml-[13%] mr-[13%]'>
                     <div class='md:flex items-center justify-between'>
                         <div class='w-[full] h-[fit] md:flex gap-x-[4vh] items-center mt-[51px]'>
                             <h1 class='w-[125px] h-[fit] leading-[44px] text-salad text-[28px]'>How can we help you?</h1>
@@ -36,12 +36,12 @@ export default function Header() {
                         </div>
                         <div class='w-[800px] h-[375px] bg-header-block bg-no-repeat bg-cover bg-center bottom-[8vh] left-[21.7vh] relative z-2 md:flex-col justify-center items-center text-white'>
                             <p class='w-fit h-fit pl-[8vh] pt-[2vh] text-[24px]'>Meet our Writers</p>
-                            <p class='w-[306px] h-[fit] pl-[8vh] pt-[2vh] pb-[1vh] text-[14px]'>We've got a professional team of professional photographers providing you with resources.</p>
-                            <div class='w-[full] h-[full] md:flex pl-[8vh]'>
-                                <img src={headerhuman1} alt="" class='w-[200px] h-[200px] relative z-3 left-[-4vh]' />
-                                <img src={headerhuman2} alt="" class='w-[200px] h-[200px] relative z-4 left-[-18vh] top-[3vh]' />
-                                <img src={headerhuman3} alt="" class='w-[200px] h-[200px] relative z-3 left-[-30vh]' />
-                                <button class='w-[146px] h-[50px] box bg-salad text-[15px] relative right-[38vh] top-[6vh] z-5'>meet our team</button>
+                            <p class='w-[306px] h-fit pl-[8vh] pt-[2vh] pb-[1vh] text-[14px]'>We've got a professional team of professional photographers providing you with resources.</p>
+                            <div class='w-full h-full md:flex pl-[8vh] mt-[3vh]'>
+                                <img src={headerhuman1} alt="" class='w-[80px] h-[80px] relative z-3' />
+                                <img src={headerhuman2} alt="" class='w-[80px] h-[80px] relative top-3 right-6 z-4' />
+                                <img src={headerhuman3} alt="" class='w-[80px] h-[80px] relative right-10 z-3' />
+                                <button class='w-[146px] h-[50px] box bg-salad text-[15px] relative shadow-2xl right-14 top-5 z-5'>meet our team</button>
                             </div>
                         </div>
                     </div>
--- vite-project/frontend/src/Mainpage/Mainpage.jsx
+++ vite-project/frontend/src/Mainpage/Mainpage.jsx
@@ -1,12 +1,49 @@
 import "../main.css"
 
+import mainfirstimg from "./imgs/mainfirstimg.png"
+import mainsecondimg from "./imgs/mainsecondimg.png"
+import mainthirdimg from "./imgs/mainthirdimg.png"
+import headerhuman3 from "../Header/imgs/headerhuman3.png"
 
-export default function Mainpage(){
+
+export default function Mainpage() {
     return (
         <>
-            <main class='w-full h-[1000px]'>
-                <div class='w-[74%] h-[700px] ml-[13%] mr-[13%]'>
-                    <h1>Recently in our Photography <span>Blog</span></h1>
+            <main class='w-full h-[1200px]'>
+                <div class='w-[60%] h-[700px] ml-[20%] mr-[20%]'>
+                    <h1 class='w-[500px] text-[52px] text-graytext'>Recently in our Photography <span class='text-orange underline'>Blog</span>.</h1>
+                    <div class='w-[1000px] h-[769px] md:flex gap-x-[5vh] mt-[3vh]'>
+                        <div class='w-[534px] h-[769px]'>
+                            <img src={mainfirstimg} alt="" />
+                            <div class='w-[400px] h-[300px] bg-white relative bottom-31 pt-[10px]'>
+                                <h2 class='w-[314px]  text-purple text-[30px]'>Photographers Who Changed the World forever</h2>
+                                <p class='w-[327px] text-[14px] text-graytext pt-[20px] pb-[20px]'>Debitis ea dicta ea eum laboriosam quis dolore repudiandae.
+                                    Est hic quod et similique ullam et. Ipsum ipsam temporibus quo ea natus
+                                    enim quae officia nulla.</p>
+                                <div class='md:flex items-center gap-x-[10px]'>
+                                    <img src={headerhuman3} alt="" class='w-[40px] h-[40px]' />
+                                    <div class='text-[14px] text-purple'>
+                                        <p>Robert Brown</p>
+                                        <p>on September 13, 2019</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class='w-fit h-[1000px] md:flex-col space-y-[20px]'>
+                            <img src={mainsecondimg} alt="" />
+                            <img src={mainthirdimg} alt="" />
+                            <div class='w-[75%] h-fit text-[18px] bg-white relative pt-[3vh] bottom-[10vh]'>
+                                <p class='text-[12px] text-graytext'>Sep 13, 2019</p>
+                                <p class='w-[222px] text-purple'>7 Things About Photography Your Boss Wants To Know</p>
+                            </div>
+                            <div class='w-[400px] h-[350px] md:flex-col space-y-[20px] pt-[25%] items-center bg-main-block bg-no-repeat bg-cover bg-center p-[60px] text-white'>
+                                <h3 class='text-[28px]'>Visit Full Blog </h3>
+                                <p class='text-[14px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <button class='text-[14px] text-orange'>Visit Blog</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
